ea8d49d11b2ed9e4df4e2c1b1b57af34c75ed915
Filip Tomanec <filip.tomanec@artio.cz>
2024-01-18 12:53:57 +0100

    Spring initialization

---
+++ src/main/java/com/filiptomanec/shiftcalendarbe/ShiftCalendarBeApplication.java
@@ -0,0 +1,13 @@
+package com.filiptomanec.shiftcalendarbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftCalendarBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftCalendarBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filiptomanec/shiftcalendarbe/ShiftCalendarBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filiptomanec.shiftcalendarbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftCalendarBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
